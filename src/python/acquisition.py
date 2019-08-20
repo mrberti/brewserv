835a3b5b76d72837e30fb4319cea497ac35145e8
Simon Bertling <berti.x@gmx.de>
2019-08-20 23:20:30 +0900

    mqttconnection is now derived from the base class

--- src/python/acquisition.py
+++ src/python/acquisition.py
@@ -1,36 +1,49 @@
+# Standard imports
 import time
 import threading
+
+# BrewServ
 from mqttconnection import MQTTConnection
 from variable import Variable
 
 class Acquisition(object):
     def __init__(self, **kwargs):
-        self._subscriptions = list()
         self._connections = list()
         self._variables = dict()
         self._thread = None
         self._is_running = threading.Event()
+        self._last_new_connection = None
+
+    def on_notify(self, notification):
+        # print(notification.connection)
+        for var in notification.connection.pop_recent_variable():
+            print(var._var["topic"], ":", var.get_last_datapoint())
 
     def new_connection(self, host, type="mqtt", port=1883, **kwargs):
-        while self._connections:
-            # Remove all connections (currently only one connection supported)
-            connection = self._connections.pop()
-            connection.disconnect()
         if type.lower() == "mqtt":
             new_connection = MQTTConnection(host, port, **kwargs)
         else:
             raise NotImplementedError("Only MQTT supported")
-        new_connection.variables = self._variables
         self._connections.append(new_connection)
+        new_connection.set_notify_cb(self.on_notify)
+        self._last_new_connection = new_connection
 
-    def new_variable(self, topic, name="", description="", unit="", **kwargs):
-        variable = Variable(topic, name, description, unit, **kwargs)
-        self._variables[topic] = variable
+    def new_variable(self, topic, connection=None, **kwargs):
+        if not connection:
+            connection = self._last_new_connection
+        var = connection.add_variable(topic, **kwargs)
+        self._variables[topic] = var
+        return var
 
     def connect(self):
         for connection in self._connections:
-            # connection.variables = self._variables
-            connection.connect()
+            try:
+                connection.connect()
+            except ConnectionError as exc:
+                print("WARNING. Could not connect to '{}'. Removing from "
+                      "connection list. {}"
+                      .format(connection, exc))
+                self._connections.remove(connection)
 
     def acquire_forever(self):
         for connection in self._connections:
@@ -58,17 +71,27 @@ class Acquisition(object):
         self._thread.join()
         self._thread = None
 
-        # Saving all data
-        for var in self._variables.values():
-            var.save()
-
     def get_variables(self):
         return list(self._variables.values())
+
+    def load_variables(self):
+        print("Loading variables")
+        for var in self._variables.values():
+            var.load()
+
+    def save_variables(self):
+        print("Saving variables")
+        for var in self._variables.values():
+            var.save()
+            var.save(type="csv")
 
 def main_test():
     daq = Acquisition()
     daq.new_connection("192.168.1.80", user="simon", password="supipass")
     daq.new_variable("test/esp32-001/temp")
+    daq.new_connection("test.mosquitto.org")
+    daq.new_variable("room/bme280")
+    daq.load_variables()
     daq.connect()
     # daq.acquire_forever()
     daq.loop_start()
@@ -79,6 +102,7 @@ def main_test():
             print("KeyboardInterrupt")
             break
     daq.loop_stop()
+    daq.save_variables()
 
 if __name__ == "__main__":
     main_test()
--- src/python/mqttconnection.py
+++ src/python/mqttconnection.py
@@ -7,28 +7,31 @@ import threading
 import paho.mqtt.client as mqtt
 
 # BrewServ
+from connection import MetaConnection
 from variable import Variable
 
 
-class MQTTConnection(object):
-    def __init__(self, server, port=1883, **kwargs):
+class MQTTConnection(MetaConnection):
+    def __init__(
+            self,
+            server,
+            port=1883,
+            user=None,
+            password=None,
+            client_id=None,
+            **kwargs):
+        MetaConnection.__init__(self, **kwargs)
         self.server = server
         self.port = port
-        self.user = kwargs.get("user")
-        self.password = kwargs.get("password")
-        self.id = kwargs.get("id", uuid.uuid4)
-        self.variables = dict()
-        self._client = mqtt.Client()
-        self._client.on_connect = self.on_connect
-        self._client.on_message = self.on_message
-        self._has_new_data_event = threading.Event()
-        self._recent_write_variables = list()
+        self.user = user
+        self.password = password
+        self.client_id = client_id if client_id else uuid.uuid4
+        self._mqtt_client = mqtt.Client()
+        self._mqtt_client.on_connect = self._on_connect
+        self._mqtt_client.on_message = self._on_message
 
-    def subscribe_to_variables(self):
-        for variable in self.variables:
-            self.subscribe(variable)
-
-    def on_connect(self, client, userdata, flags, rc):
+    # MQTT specific functions
+    def _on_connect(self, client, userdata, flags, rc):
         if rc == 0:
             print("{}: Connected to {}:{}. ({})."
                   .format(self, self.server, self.port, rc))
@@ -38,60 +41,62 @@ class MQTTConnection(object):
                   "password ({})."
                   .format(self, self.server, self.port, rc))
         else:
-            raise Exception("Could not connect")
+            raise ConnectionError("Could not connect")
 
-    def on_message(self, client, userdata, msg):
-        if msg.topic in self.variables:
-            vvv = self.variables[msg.topic]
-            vvv.push_data(msg.payload.decode("utf-8"))
-            self._has_new_data_event.set()
-            self._recent_write_variables.append(vvv)
+    def _on_message(self, client, userdata, msg):
+        topic = msg.topic
+        data = msg.payload.decode("utf-8")
+        self.push_data(topic, data)
 
-    def has_new_data(self):
-        return self._has_new_data_event.is_set()
+    def __str__(self):
+        # d = dict()
+        # d["server"] = self.server
+        # d["port"] = self.port
+        # d["user"] = self.user
+        # d["password"] = self.password
+        # d["client_id"] = self.client_id
+        # return str(d)
+        return "{!r} {}:{}".format(self, self.server, self.port)
 
-    def wait_for_new_data(self, timeout=None):
-        self._has_new_data_event.wait(timeout)
-
-    def pop_recent_variable(self):
-        if self._recent_write_variables:
-            var = self._recent_write_variables.pop(0)
-            if not self._recent_write_variables:
-                self._has_new_data_event.clear()
-            yield var
-        else:
-            self._has_new_data_event.clear()
-
+    # Implementing the abstract methods
     def connect(self):
         if self.user:
-            self._client.username_pw_set(self.user, self.password)
-        self._client.connect(self.server)
+            self._mqtt_client.username_pw_set(self.user, self.password)
+        try:
+            self._mqtt_client.connect(self.server)
+        except TimeoutError as exc:
+            raise ConnectionError(exc)
 
     def start(self):
-        self._client.loop_start()
-
+        self._mqtt_client.loop_start()
+    
+    def subscribe(self, topic, qos=0):
+        self._mqtt_client.subscribe(topic, qos)
+    
     def stop(self):
-        self._client.loop_stop()
+        self._mqtt_client.loop_stop()
 
     def disconnect(self):
-        self._client.disconnect()
+        self._mqtt_client.disconnect()
 
-    def subscribe(self, topic, qos=0):
-        self._client.subscribe(topic, qos)
+
+def got_notified(notification):
+    # connection.wait_for_new_data(1.)
+    print(notification)
+    for var in notification.connection.pop_recent_variable():
+        print(var._var["topic"], ":", var.get_last_datapoint())
+        pass
 
 def main_test():
     server = MQTTConnection("192.168.1.80", user="simon", password="supipass")
-    server.variables["test/esp32-001/temp"] = Variable("test/esp32-001/temp")
-    server.variables["test/esp32-001/hall"] = Variable("test/esp32-001/hall")
+    server.add_variable("test/esp32-001/temp")
+    # server.add_variable("test/esp32-001/hall")
+    server.set_notify_cb(got_notified)
     server.connect()
-    server.subscribe("test/#")
     server.start()
     while True:
         try:
-            server.wait_for_new_data()
-            for var in server.pop_recent_variable():
-                pass
-                print(var._var["topic"])
+            time.sleep(1)
         except KeyboardInterrupt:
             break
     server.stop()
