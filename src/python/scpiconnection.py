74058ed9f79766831459a9cb421377303477ee57
Simon Bertling <berti.x@gmx.de>
2019-08-20 23:20:56 +0900

    added stub classes for further connection handlers

---
+++ src/python/scpiconnection.py
@@ -0,0 +1,3 @@
+class SCPIConnection(object):
+    def __init__(self):
+        raise NotImplementedError("This class is a stub")
---
+++ src/python/serialconnection.py
@@ -0,0 +1,3 @@
+class SerialConnection(object):
+    def __init__(self):
+        raise NotImplementedError("This class is a stub")
